e962c51ca0785df4eb2ee3f54dd98d0a4cd2b7b4
HSKKOU <hskk1120551@gmail.com>
2016-08-21 16:50:09 +0900

    ready admin view by copied from index

--- module/Application/src/Application/Controller/AdminController.php
+++ module/Application/src/Application/Controller/AdminController.php
@@ -1,28 +1,30 @@
 <?php
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class AdminController extends AbstractRPController
 {
   public function getList()
   {
-    return new ViewModel();
+    return new JsonModel();
   }
 
   public function get($room_id)
   {
-    return new ViewModel();
+    return new JsonModel(array(
+      'room_id' => $room_id,
+    ));
   }
 
   public function create($data)
   {
-    return new ViewModel();
+    return new JsonModel();
   }
 
 
-  public function adminAction()
+  public function indexAction()
   {
     $viewAliases = array(
       'code_view',
